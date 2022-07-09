b850a9fa3665ae48c7f8e55f03f62bda018d6935
vspl3sk <vspl3sk@yandex.ru>
2022-07-09 15:43:11 +0300

    Зачада на определение максимального из трех чисел

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int third_num = Convert.ToInt32(Console.ReadLine());
+int max = first_num;
+if (max < second_num){
+    max = second_num;
+}
+if (max < third_num){
+    max = third_num;
+}
+Console.WriteLine($"Большее из этих чисел {max}");
